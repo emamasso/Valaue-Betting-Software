19567a9e977ed9e8614340744155604ddb8786ed
emamasso <emanuelemas@icloud.com>
2026-03-10 19:42:04 +0100

    Worked on getting predictions for upcoming matches

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import pickle
 import joblib
+import numpy as np
 
 data = pd.read_csv('prediction_data/final_data.csv', sep = ';')
 
-data['home_is_home'] = 1
+data['home_is_home'] = 1 
 data['away_is_home'] = 0
 
 data_to_predict = data[['home_is_home', 
@@ -22,15 +23,7 @@ data_to_predict = data[['home_is_home',
 
 
 
-df = pd.read_csv('data/final_data.csv', sep=';')
 
-X = df[['home_is_home', 'B365H', 'B365D', 'B365A',
-        'home_rest_days','home_total_goals','home_total_xg',
-        'home_total_goals_against','home_total_xg_against','home_last_5',
-        'home_PPG','away_is_home','away_rest_days',
-        'away_total_goals','away_total_xg',
-        'away_total_goals_against','away_total_xg_against',
-        'away_last_5','away_PPG']]
 
 with open('model_v2.pkl', 'rb') as file:
     model = pickle.load(file)
@@ -42,8 +35,42 @@ scaler = joblib.load('scaler.pkl')
 
 scaled_data = scaler.transform(data_to_predict)
 
-predictions = model.predict(scaled_data)
-print(len(predictions))
-print(predictions)
+predictions = list(model.predict(scaled_data))
+probabilities =list(model.predict_proba(scaled_data))
+
+games = []
+
+for i in range(data.shape[0]):
+    games.append('-'.join((data['home'].loc[i], data['away'].loc[i])))
 
 
+
+dictionary = {'Game':games, 'Forecasted result':predictions}
+
+
+final_data_frame = pd.DataFrame(dictionary)
+
+
+mask = [final_data_frame['Forecasted result'] == 0,
+        final_data_frame['Forecasted result'] == 1,
+        final_data_frame['Forecasted result'] == 2]
+
+final_data_frame['Forecasted result'] = np.select(mask, ['1', 'X', '2'], default='N/A')
+
+
+prob = [max(x) for x in probabilities]
+
+final_data_frame['Probability'] = prob
+
+
+odds = pd.read_csv('prediction_data/games_odds.csv', sep = ';')
+
+final_data_frame['Home Win'] = odds['H']
+final_data_frame['Draw'] = odds['A']
+final_data_frame['Away Win'] = odds['A']
+
+quote_cols = ['Away Win', 'Draw', 'Home Win']
+final_data_frame['Expected Value'] = final_data_frame['Probability'] * final_data_frame[quote_cols].min(axis=1)
+
+
+print(final_data_frame[final_data_frame['Expected Value'] >= 1.05].sort_values(by=['Expected Value'], ascending=False))
